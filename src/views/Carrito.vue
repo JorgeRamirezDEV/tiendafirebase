c88b69197ee22cb6989d447f86b0ffa37080eff2
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-02-20 01:59:29 +0100

    .

---
+++ src/views/Carrito.vue
@@ -0,0 +1,15 @@
+<template>
+  <carrito/>
+</template>
+
+<script>
+
+import carrito from '@/components/carrito.vue'
+
+export default {
+  name: 'Carrito',
+  components: {
+    carrito
+  }
+}
+</script>
