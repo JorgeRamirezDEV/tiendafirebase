b1f8201a22d8a15d2048eea1e603db3616a78ba0
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-02-21 02:03:59 +0100

    .

--- src/components/addproductos.vue
+++ src/components/addproductos.vue
@@ -2,7 +2,7 @@
 
   <section class="anadirproducto">
     <div class="shadow p-3 m-5 bg-white rounded mx-auto border-primary border-bottom" style="max-width: 1000px">
-        <h1> Subir producto </h1>
+        <h1 class="mb-5"> Subir producto </h1>
         <form>
           <div class="form-group mb-5">
             <label>Nombre del producto</label>
@@ -37,12 +37,58 @@
           </div>
         </form>
     </div>
+    <div class="shadow p-3 m-5 bg-white rounded mx-auto border-primary border-bottom" style="max-width: 1000px">
+      <h1 class="my-3"> Editar productos </h1>
+      <h2> Sección de Ofertas </h2>
+    <div class="row my-5 pb-5 mx-0 justify-content-center border-bottom">
+      <div class="shadow-sm p-3 m-2 bg-white rounded border" v-for="novedad in datos" v-bind:key="novedad.id">
+          <div>   
+            <h2 class="mt-0">{{novedad.nombre}}</h2>
+          <div class="my-3">
+            <h5> Precio </h5> 
+            <input type="text" v-bind:value='novedad.precio'>
+            <a class="btn btn-success mx-2 p-1">Actualizar</a>
+          </div>
+          <div class="my-3">
+            <h5> Stock </h5> 
+            <input type="text" v-bind:value='novedad.stock'>
+            <a class="btn btn-success mx-2 p-1">Actualizar</a>
+          </div>
+          <div class="my-3">
+            <a class="btn btn-danger mx-2 p-1">Eliminar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2> Sección de Productos </h2>
+    <div class="row my-5 mx-0 justify-content-center">
+      <div class="shadow-sm p-3 m-2 bg-white rounded border" v-for="consola in datos2" v-bind:key="consola.id">
+          <div>   
+            <h2 class="mt-0">{{consola.nombre}}</h2>
+          <div class="my-3">
+            <h5> Precio </h5> 
+            <input type="text" v-bind:value='consola.precio'>
+            <a class="btn btn-success mx-2 p-1">Actualizar</a>
+          </div>
+          <div class="my-3">
+            <h5> Stock </h5> 
+            <input type="text" v-bind:value='consola.stock'>
+            <a class="btn btn-success mx-2 p-1">Actualizar</a>
+          </div>
+          <div class="my-3">
+            <a class="btn btn-danger mx-2 p-1">Eliminar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
   </section>
 
 </template>
 
 <script lang="js">
 
+  import {db} from '../db.js'
   export default  {
     name: 'anadirproducto',
     props: [],
@@ -51,6 +97,8 @@
     },
     data () {
       return {
+        datos:[],
+        datos2:[]
 
       }
     },
@@ -59,6 +107,11 @@
     },
     computed: {
 
+    },
+
+    firestore: {
+      datos:db.collection('ofertas'),
+      datos2:db.collection('consolas')
     }
 }
 
